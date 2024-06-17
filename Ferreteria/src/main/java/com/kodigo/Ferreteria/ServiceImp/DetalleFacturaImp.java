21659ce3df526f37da618f9987a63ad06166b335
ROLANDO ANDRADE <2712072023@mail.utec.edu.sv>
2024-06-16 20:50:32 -0600

    Created ServiceImplementations

---
+++ Ferreteria/src/main/java/com/kodigo/Ferreteria/ServiceImp/DetalleFacturaImp.java
@@ -0,0 +1,33 @@
+package com.kodigo.Ferreteria.ServiceImp;
+
+import com.kodigo.Ferreteria.DAO.DetalleFacturaDao;
+import com.kodigo.Ferreteria.Service.DetalleFacturaService;
+import com.kodigo.Ferreteria.entity.DetalleFacturaEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class DetalleFacturaImp implements DetalleFacturaService {
+    @Autowired
+    private DetalleFacturaDao detalleFacturaDao;
+    @Override
+    public List<DetalleFacturaEntity> listDetalleFactura() {
+        return detalleFacturaDao.findAll();
+    }
+
+    @Override
+    public DetalleFacturaEntity createDetalleFactura(DetalleFacturaEntity detalleFacturaEntity) {
+        return detalleFacturaDao.save(detalleFacturaEntity);
+    }
+
+    //missing Update
+    @Override
+    public DetalleFacturaEntity updateDetalleFactura(DetalleFacturaEntity detalleFacturaEntity) {
+        return null;
+    }
+
+    @Override
+    public void deleteDetalleFactura(Integer id) { detalleFacturaDao.deleteById(id);
+
+    }
+}
